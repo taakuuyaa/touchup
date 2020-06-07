8d505a8fc88fb4fba70fe7609637a7d638b1f344
taakuuyaa <51745522+taakuuyaa@users.noreply.github.com>
2020-06-08 00:30:59 +0900

    Install apartment

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -16,7 +16,7 @@ Apartment.configure do |config|
   # Add any models that you do not want to be multi-tenanted, but remain in the global (public) namespace.
   # A typical example would be a Customer or Tenant model that stores each Tenant's information.
   #
-  # config.excluded_models = %w{ Tenant }
+  config.excluded_models = %w{ Tenant }
 
   # In order to migrate all of your Tenants you need to provide a list of Tenant names to Apartment.
   # You can make this dynamic by providing a Proc object to be called on migrations.
@@ -48,7 +48,7 @@ Apartment.configure do |config|
   #   end
   # end
   #
-  config.tenant_names = lambda { ToDo_Tenant_Or_User_Model.pluck :database }
+  config.tenant_names = lambda { Tenant.pluck :name }
 
   # PostgreSQL:
   #   Specifies whether to use PostgreSQL schemas or create a new database per Tenant.
@@ -107,3 +107,4 @@ end
 Rails.application.config.middleware.use Apartment::Elevators::Subdomain
 # Rails.application.config.middleware.use Apartment::Elevators::FirstSubdomain
 # Rails.application.config.middleware.use Apartment::Elevators::Host
+Apartment::Elevators::Subdomain.excluded_subdomains = %w[www app]
