0bac9adda645c67e76be1efa73df049e3ec07f67
taakuuyaa <51745522+taakuuyaa@users.noreply.github.com>
2020-06-18 15:31:12 +0900

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+# port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -35,3 +35,6 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+# nginxとのソケット通信用
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
