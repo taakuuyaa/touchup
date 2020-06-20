6df95025b4566ff3034025a74f053f4a4115a421
taakuuyaa <51745522+taakuuyaa@users.noreply.github.com>
2020-06-20 19:48:35 +0900

    Update sessions_helper.rb

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,28 +2,28 @@ module SessionsHelper
 
   def login_in(tenant)
     remember_token = Tenant.new_remember_token
-    cookies.permanent[:remember_token] = {
-        value: remember_token,
-        domain: :all,
-        tld_length: 2
-    }
+    # TODO 有効期限設定
+    cookies.permanent[:remember_token] = remember_token
     tenant.update_attribute(:remember_token, Tenant.encrypt(remember_token))
     self.current_tenant = tenant
+  end
+
+  def log_out
+    cookies.delete(:remember_token)
+    @current_tenant = nil
   end
 
   def current_tenant=(tenant)
     @current_tenant = tenant
   end
 
-  # signed_in?を経由して、セッションが張られているかどうかを確認する
-  # つまり、（ほとんどの）リクエストの度に呼ぶようにする。
   def current_tenant
     remember_token = Tenant.encrypt(cookies[:remember_token])
     @current_tenant ||= Tenant.find_by(remember_token: remember_token)
   end
 
   def login_in?
-    !current_tenant.nil? && request.subdomain == current_tenant.subdomain
+    current_tenant.present?
   end
 
   def destroy
@@ -32,7 +32,7 @@ module SessionsHelper
   end
 
   def require_login_in!
-    redirect_to subdomain: 'app', :controller => 'sessions', :action => "new" unless login_in?
+    redirect_to :controller => 'sessions', :action => "new" unless login_in?
   end
 
 end
