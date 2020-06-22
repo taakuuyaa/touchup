3e3813069ab2fbd7d00c722e3027e8043774818a
taakuuyaa <51745522+taakuuyaa@users.noreply.github.com>
2020-06-22 23:04:43 +0900

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,2 @@
-Rails.application.config.session_store :cookie_store, key: '_onstaff_session', domain: :all
+session_name = Rails.env.production? ? '_onstaff_session' : "_onstaff_#{Rails.env}_session"
+Rails.application.config.session_store :cookie_store, key: session_name, expire_after: 24.hours, domain: Settings.session.domain, secure: Rails.env.production?
