717997702cf199d012994f9c0ce42dca674e6664
taakuuyaa <51745522+taakuuyaa@users.noreply.github.com>
2020-06-30 00:42:22 +0900

    fix video.rb

--- app/models/video.rb
+++ app/models/video.rb
@@ -7,13 +7,12 @@ class Video < ApplicationRecord
 
   mount_uploader :file, VideoUploader
 
-  enum status: {published: 0, closed: 1, deleted: 3}
+  enum status: {published: 0, closed: 1, deleted: 2}
 
   def self.save_from_request(request)
     # video_items = request.delete(:video_items)
     video = new(request)
     video.capacity = video.file.size
-
 
     if video.save
       video.save_video_items('')
