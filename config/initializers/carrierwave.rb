dc5105c66362cf1f0e20fab4ce83d8abace0af51
taakuuyaa <51745522+taakuuyaa@users.noreply.github.com>
2020-07-05 20:38:39 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,10 +3,6 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage :fog
-  config.cache_storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_directory  = "video-upload.#{Rails.env}.onstaff"
   config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
@@ -14,6 +10,28 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1',
       path_style: true
   }
+
+  config.storage :fog
+  config.cache_storage = :fog
+  config.fog_provider = 'fog/aws'
+
+  case Rails.env
+  when 'production'
+    config.fog_directory = 'video-upload.production.onstaff'
+    # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/dummy'
+
+  when 'staging'
+    config.fog_directory = 'video-upload.staging.onstaff'
+    # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/dummy'
+
+  when 'development'
+    config.fog_directory = 'video-upload.development.onstaff'
+    # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/dev.dummy'
+
+  else
+    config.fog_directory = nil
+  end
+
 end
 
 # 日本語ファイル名の許可
