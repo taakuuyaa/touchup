08e875a6eeed2e16fa3b4790c7689a00792c47c2
taakuuyaa <51745522+taakuuyaa@users.noreply.github.com>
2020-07-05 20:30:32 +0900

    update video.rb

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,23 +1,16 @@
 class Video < ApplicationRecord
+  has_one :video_capacity
+  has_many :video_item_links, dependent: :destroy
+  accepts_nested_attributes_for :video_item_links, allow_destroy: true
+
+  validates_associated :video_item_links
+
   with_options presence: true do
     validates :item_url, uniqueness: true
     validates :file
-    validates :capacity
   end
 
   mount_uploader :file, VideoUploader
 
-  enum status: {published: 0, closed: 1, deleted: 2}
-
-  def self.get_all_capacity
-    Video.all.sum(:capacity) / 1.gigabyte
-  end
-
-  def save_video_items(request_video_items)
-    true
-    # return true if request_video_items.blank?
-    #
-    # self.video_items.delete_all
-    # request_video_items.all? { |vi| self.video_items.create(video_id: self.id, item_text: vi.item_text, item_url: vi.item_url) }
-  end
+  enum status: {published: 0, closed: 1}
 end
