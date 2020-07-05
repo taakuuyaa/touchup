af874107e79d796e3823fd7f9fd955aa4f72505e
taakuuyaa <51745522+taakuuyaa@users.noreply.github.com>
2020-07-05 16:20:35 +0900

    Update データ量計算

--- app/models/video.rb
+++ app/models/video.rb
@@ -9,16 +9,8 @@ class Video < ApplicationRecord
 
   enum status: {published: 0, closed: 1, deleted: 2}
 
-  def self.save_from_request(request)
-    # video_items = request.delete(:video_items)
-    video = new(request)
-    video.capacity = video.file.size
-
-    if video.save
-      video.save_video_items('')
-    end
-
-    video
+  def self.get_all_capacity
+    Video.all.sum(:capacity) / 1.gigabyte
   end
 
   def save_video_items(request_video_items)
