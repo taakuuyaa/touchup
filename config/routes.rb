e386917f0f753837a0b39093ffd710e93740cc44
taakuuyaa <51745522+taakuuyaa@users.noreply.github.com>
2020-08-17 22:04:38 +0900

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,13 @@ Rails.application.routes.draw do
 
   constraints ExcludedSubdomainConstraint do
     root to: redirect('/movie')
-
     # ログイン関連
     get 'login' => 'sessions#new'
     post 'login' => 'sessions#create'
     delete 'logout' => 'sessions#destroy'
-
     # 動画
-    get 'movie' => 'videos#index'
-    get 'movie/add' => 'videos#new'
-    post 'movie' => 'videos#create'
-    delete 'movie/:id' => 'videos#destroy'
-
+    resources :movies, only: [:index, :new, :create, :destroy], controller: 'videos'
+    # 動画ステータス変更
     put 'movie/:id/status' => 'videos#change_status'
   end
 end
